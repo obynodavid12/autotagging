fe9b4b273e3d15d0466a106a6b4d99b00bb3156b
obinna <obynodavid12@gmail.com>
2022-11-12 23:13:14 -0500

    removed v

--- releash.sh
+++ releash.sh
@@ -4,7 +4,7 @@
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 # v1.0.0, v1.5.2, etc.
-versionLabel=v$1
+versionLabel=$1
 
 # establish branch and tag name variables
 #devBranch=develop       #commented here
