abb5749dc112b6ddcff74901248f84ee2475f471
obinna <obynodavid12@gmail.com>
2022-11-13 13:50:17 -0500

    updatetags

--- updatetags.sh
+++ updatetags.sh
@@ -9,10 +9,10 @@ echo "================================="
 for tag in "${tags[@]}"
 do
   hash=${commit_hashes[COUNTER]}
-  if [[ "${tag}" != v3.8.12.8-* ]]
+  if [[ "${tag}" != cloud-* ]]
   then
-    echo "Add new tag 'v3.8.12.8-${tag}' to local repo on commit ${hash}"
-    eval "git tag v3.8.12.8-${tag} ${hash}"
+    echo "Add new tag 'cloud-${tag}' to local repo on commit ${hash}"
+    eval "git tag cloud-${tag} ${hash}"
 
     echo "Delete old tag '${tag}' from local"
     eval "git tag --delete ${tag}"
