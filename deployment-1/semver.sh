1b148a89e02c9da67b2ee33bc331882c973fbd95
obinna <obynodavid12@gmail.com>
2022-11-15 16:24:35 -0500

    updated semver with fetch and pull

--- deployment-1/semver.sh
+++ deployment-1/semver.sh
@@ -4,12 +4,13 @@
 VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [ -z $VERSION ];then
-    NEW_TAG=""
+    NEW_TAG="y1b-3.8.11.1.0"
+    RELEASEDATE=$(date '+%Y%m%d')
     echo "No tag present."
-    echo "Creating tag: $NEW_TAG"
-    git tag $NEW_TAG 
+    echo "Creating tag: $NEW_TAG_$RELEASEDATE"
+    git tag $NEW_TAG_$RELEASEDATE 
     git push --tags
-    echo "Tag created and pushed: $NEW_TAG"
+    echo "Tag created and pushed: $NEW_TAG_$RELEASEDATE"
     exit 0;
 fi
 
@@ -25,7 +26,7 @@ VNUM5=${VERSION_BITS[4]}
 VNUM5=$((VNUM5+1))
 
 #create new tag
-NEW_TAG="${VNUM1}.${VNUM2}.${VNUM3}.${VNUM4}.${VNUM5}-hotfix"
+NEW_TAG="${VNUM1}.${VNUM2}.${VNUM3}.${VNUM4}.${VNUM5}"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
@@ -34,9 +35,9 @@ CURRENT_COMMIT_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
 if [ -z "$CURRENT_COMMIT_TAG" ]; then
     echo "Updating $VERSION to $NEW_TAG"
-    git tag $NEW_TAG
+    git tag $NEW_TAG_$RELEASEDATE
     git push --tags
-    echo "Tag created and pushed: $NEW_TAG"
+    echo "Tag created and pushed: $NEW_TAG_$RELEASEDATE"
 else
     echo "This commit is already tagged as: $CURRENT_COMMIT_TAG"
 fi
