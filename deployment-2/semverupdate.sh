812e7e1c94931667006e7c54eb78ea066fd96c55
obinna <obynodavid12@gmail.com>
2022-11-17 09:22:27 -0500

    changes in semverupdate.sh

--- deployment-2/semverupdate.sh
+++ deployment-2/semverupdate.sh
@@ -42,8 +42,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 #only tag if no tag already
 #to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
 if [ -z "$NEEDS_TAG" ]; then
-  npm version $NEW_TAG
-  npm publish --access public
+  git tag -a $NEW_TAG
   echo "Tagged with $NEW_TAG"
   git push --tags
   git push
